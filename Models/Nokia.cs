fb9762ce1ec3bd2db9c7622e6144f056bae92c06
ALSAN <125503777+ALSANhub@users.noreply.github.com>
2025-07-27 10:46:04 -0300

    Projeto concluído: Projeto Celular com classes Smartphone, Nokia e Iphone

--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -1,8 +1,17 @@
 namespace DesafioPOO.Models
 {
     // TODO: Herdar da classe "Smartphone"
-    public class Nokia
+    public class Nokia : Smartphone
     {
-        // TODO: Sobrescrever o método "InstalarAplicativo"
+        public Nokia(string numero, string modelo, string imei, int memoria)
+            : base(numero, modelo, imei, memoria) { }
+
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Instalando o aplicativo {nomeApp} no Nokia.");
+            // Lógica específica para instalar o aplicativo no Nokia
+            // Por exemplo, poderia haver uma verificação de compatibilidade ou algo do tipo
+            Console.WriteLine($"Aplicativo {nomeApp} instalado com sucesso no Nokia.");
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,22 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+class Program
+{
+    static void Main(string[] args)
+    {
+        Smartphone meuNokia = new Nokia("123456789", "Modelo Nokia", "Versão 1.0", 64);
+        Smartphone meuIphone = new Iphone("987654321", "Modelo iPhone", "Versão 2.0", 256);
+
+        Console.WriteLine("Instalando aplicativos nos smartphones:");
+        meuNokia.Ligar();
+        meuNokia.ReceberLigacao();
+        meuNokia.InstalarAplicativo("WhatsApp");
+
+        Console.WriteLine();
+
+        Console.WriteLine("Instalando aplicativos no iPhone:");
+        meuIphone.Ligar();
+        meuIphone.ReceberLigacao();
+        meuIphone.InstalarAplicativo("Instagram");
+    }   
+}
